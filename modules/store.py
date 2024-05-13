562437b768099010cb6376575d6aa2d390748c40
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-05-13 07:46:54 +0200

    storefront (mtxShop missing)

--- modules/store.py
+++ modules/store.py
@@ -6,6 +6,8 @@ import json
 script_dir = os.path.dirname(__file__)
 parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
 
+# storefront, this is for the item shop, if you want you can add the vbucks shop response to have them display, no need to really :/
+
 def catalog():
     path = os.path.join(parent_dir, "responses/store/catalog.json")
     with open(path, "r", encoding="utf-8") as catalogentry:
