5ddcd1320a529df8679d7dea459caaee1ed900ab
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-05-13 07:43:52 +0200

    someone PR this and fix this fucking logic

--- modules/account.py
+++ modules/account.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 import os
 import json
 
+#account logic very janky, do not touch if you don't know what this is!
+
 script_dir = os.path.dirname(__file__)
 parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
 
