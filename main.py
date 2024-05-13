5741863f615eaaaa6ce1275bb1f2dcecabb45753
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-05-13 07:21:43 +0200

    S3 logic

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ def fortnitegameresponse():
     elif "1.11" or "2.1.0" or "2.2.0" or "2.3.0" or "2.4.0" or "2.4.2" or "2.5.0" in current_season:
         #replace with ch1 s2 response
         return fortnitegame()
+    elif "3.0" or "3.1" or "3.2" or "3.3" or "3.5" or "3.6" in current_season:
+        #replace with ch1 s3 response
+        return fortnitegame()
 
 @app.route("/datarouter/api/v1/public/data", methods=["POST"])
 def datarouter():
